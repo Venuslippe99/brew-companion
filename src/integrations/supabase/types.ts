410ef765f0bf9140545080ac7058f2994dd76193
Venuslippe99 <137096511+Venuslippe99@users.noreply.github.com>
2026-03-21 18:38:38 +0100

    Update types.ts to include F2

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -22,6 +22,7 @@ export type Database = {
           title: string | null
           updated_at: string
           user_id: string
+          f2_started_at: string | null
         }
         Insert: {
           created_at?: string
@@ -30,6 +31,7 @@ export type Database = {
           title?: string | null
           updated_at?: string
           user_id: string
+          f2_started_at?: string | null
         }
         Update: {
           created_at?: string
@@ -38,6 +40,7 @@ export type Database = {
           title?: string | null
           updated_at?: string
           user_id?: string
+          f2_started_at?: string | null
         }
         Relationships: [
           {
